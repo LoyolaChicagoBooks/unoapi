54026739454ba651a711ad71af19ba576f58bd51
George K. Thiruvathukal <gkt@cs.luc.edu>
2023-11-25 23:26:05 -0600

    clean up HTML version info

--- source/conf.py
+++ source/conf.py
@@ -149,7 +149,7 @@ html_theme_options = {
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
-html_title = " ".join([project +  ": updated", release])
+html_title = " ".join([project, release])
 
 # A shorter title for the navigation bar.  Default is the same as html_title.
 #html_short_title = "UnoAPI <release>"
