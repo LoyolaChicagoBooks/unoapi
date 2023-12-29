960ed94221a878b772272bb555d50d533a23a581
George K. Thiruvathukal <gkt@cs.luc.edu>
2023-12-29 07:15:11 +0000

    make script a bit more relocatable for other projects

--- scripts/gkt-remote-tablet.sh
+++ scripts/gkt-remote-tablet.sh
@@ -4,13 +4,16 @@ if [ -f ~/zenv/bin/activate ]; then
    . ~/zenv/bin/activate
 fi
 
+REPO_NAME=unoapi
 # Build HTML and PDF versions
 make html && make latexpdf
 
-if [ -d ~/public_html/unoapi ]; then
-   rsync -avz ./build/ ~/public_html/unoapi
+if [ -d ~/public_html/${REPO_NAME} ]; then
+   rsync -avz ./build/ ~/public_html/${REPO_NAME}
 else
    echo "No personal web storage found. Skipping."
 fi
 
-rclone copy build/latex/UnoAPI.pdf office-luc:gdemo
+find build/latex -name '*.pdf' \
+	-exec echo rclone copy {} office-luc:gdemo \; \
+	-exec rclone copy {} office-luc:gdemo \; 
